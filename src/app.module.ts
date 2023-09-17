201eb31998d941886c15e70a77cb9c22a4b28335
Yamir Minaya <yamir.minaya@gmail.com>
2023-09-16 20:49:18 -0500

    Last changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { JoiValidationSchema } from './config/joi.validation';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot(process.env.MONGODB),
+    MongooseModule.forRoot(process.env.MONGODB, { dbName: 'pokemonsdb' }),
     PokemonModule,
     CommonModule,
     SeedModule,
